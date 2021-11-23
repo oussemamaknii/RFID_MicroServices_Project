6463f898bf18d839986d94d924be582ea74e0e2d
HasseneING <mohamedhassene.hamed@esprit.tn>
2021-11-23 10:11:40 +0000

    Book Micro service added
    -Missing template implementation
    -Missing mongoDB linking

---
+++ web app/BookMicroService/Book/src/main/java/com/esprit/microservice/BookRestAPI.java
@@ -0,0 +1,41 @@
+package com.esprit.microservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping(value = "/api/books")
+public class BookRestAPI {
+@Autowired
+private BookService bookService;
+@PostMapping
+@ResponseStatus(HttpStatus.CREATED)
+public ResponseEntity<Book> createBook(@RequestBody Book book) {
+return new ResponseEntity<>(bookService.addBook(book), HttpStatus.OK);
+}
+@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Book> updateBook(@PathVariable(value = "id") int id,
+    @RequestBody Book book){
+return new ResponseEntity<>(bookService.updateBook(id, book), HttpStatus.OK);
+}
+@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteBook(@PathVariable(value = "id") int id){
+return new ResponseEntity<>(bookService.deleteBook(id), HttpStatus.OK);
+}
+}
