9e9175e1d7d34d37f27b1c71358c1c6bc8387eeb
AhmedKolsi98 <ahmedkolsi98@gmail.com>
2021-11-26 10:45:18 +0100

    app categorie catalog

--- axelib_auth/lib/src/screens/dummy_data.dart
+++ axelib_auth/lib/src/screens/dummy_data.dart
@@ -5,52 +5,52 @@ import './models/category.dart';
 const DUMMY_CATEGORIES = const [
   Category(
     id: 'c1',
-    title: 'Italian',
+    title: 'Sci-fi',
     color: Colors.purple,
   ),
   Category(
     id: 'c2',
-    title: 'Quick & Easy',
+    title: 'Romance',
     color: Colors.red,
   ),
   Category(
     id: 'c3',
-    title: 'Hamburgers',
+    title: 'History',
     color: Colors.orange,
   ),
   Category(
     id: 'c4',
-    title: 'German',
+    title: 'Philosophy',
     color: Colors.amber,
   ),
   Category(
     id: 'c5',
-    title: 'Light & Lovely',
+    title: 'Bibliography',
     color: Colors.blue,
   ),
   Category(
     id: 'c6',
-    title: 'Exotic',
+    title: 'Biology',
     color: Colors.green,
   ),
   Category(
     id: 'c7',
-    title: 'Breakfast',
+    title: 'Children Books',
     color: Colors.lightBlue,
   ),
   Category(
     id: 'c8',
-    title: 'Asian',
+    title: 'Fantasy',
     color: Colors.lightGreen,
   ),
   Category(
     id: 'c9',
-    title: 'French',
+    title: 'Modern Litterature',
     color: Colors.pink,
   ),
   Category(
     id: 'c10',
-    title: 'Summer',
+    title: 'Other',
     color: Colors.teal,
   ),
 ];
--- axelib_auth/lib/src/screens/home.dart
+++ axelib_auth/lib/src/screens/home.dart
@@ -1,22 +1,23 @@
 import 'package:axelib_auth/src/screens/login.dart';
+import 'package:axelib_auth/src/screens/widgets/main_drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'categories_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FlatButton(
-            child: Text('Welcome'),
-            onPressed: () {
-              auth.signOut();
-              Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => LoginScreen()));
-            }),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login App',
+      theme: ThemeData(
+        accentColor : Colors.orange,
+        primarySwatch: Colors.green,
       ),
-    );
+      home: CategoriesScreen(),);
+   
   }
 }
