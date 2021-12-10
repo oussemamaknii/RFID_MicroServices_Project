cb495821eb31e3103a8f11a156ad34a54f23b4a0
ghassenelkamel <ghassenelkamel@live.fr>
2021-12-10 12:05:18 +0100

    work

--- angular-frontend/src/app/event.service.spec.ts
+++ angular-frontend/src/app/event.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { EventService } from './event.service';
 
-describe('EmployeeService', () => {
+describe('EventService', () => {
   let service: EventService;
 
   beforeEach(() => {
